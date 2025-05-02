41ec9db1928be2e23ad2eb19b432a6daebcd12e3
Chris <cjswindle1971@gmail.com>
2025-05-01 20:58:55 -0700

    Navbar Almost Done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import '../Navbar.css';
 import StoreRusLogo from "../assets/StoreRus.com logo.png";
-
+import ShoppingCart from "../assets/shopping-cart.png"
 const Navbar = () => {
   return (
 
     <nav className="navbar">
       <div className="navbar-left">
-        <a href="/" className="logo"><img src= {StoreRusLogo}/></a>
+        <a href="/" className="logo"><img src= {StoreRusLogo} width={100} height={75}/></a>
       </div>
       <div className="navbar-center">
         <ul className="nav-links">
           <li>
-            <a className="navbar-title" href="/">StoreRus</a>
+            <a id="navbar-title" href="/">StoreRus</a>
           </li>
-          <li>
-            <a href="#">Sign Up</a>
-          </li>
+          
         </ul>
       </div>
       <div className="navbar-right">
         <ul className="links-right">
           <li>
-            <a href="#" className="shopping-cart">Shopping Cart</a>
+            <a href="#" id="sign-up">Sign Up</a>
           </li>
           <li>
-            <a href="#" className="sign-in">Sign In</a>
+            <a href="#" id="sign-in">Sign In</a>
           </li>
+          <li>
+            <a href="#" id="shopping-cart"><img src={ShoppingCart} width={75} height={50}/></a>
+          </li>
+          
         </ul>
       </div>
     </nav>
