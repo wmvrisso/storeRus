0d32018e2393297f78f7fafd2ecb716024c48590
Gus <gus@macbookpro.lan>
2025-05-01 21:58:30 -0600

    added sign-up and login routes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
 import Search from './pages/search';
-import Cart from './pages/cart'
+import Cart from './pages/cart';
+import Login from './pages/login';
+import Signup from './pages/register';
 import NotFound from './pages/notfound';
 
 const router = createBrowserRouter([
@@ -19,6 +21,14 @@ const router = createBrowserRouter([
       {
         path: '/cart',
         element:<Cart />
+      },
+      {
+        path: '/login',
+        element:<Login />
+      },
+      {
+        path: '/signup',
+        element: <Signup />
       }
     ],
   },
